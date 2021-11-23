786aa0b9adc335dc1171d39cec8c6b6d96cf78e8
dominiclai1983 <dominiclai1983@gmail.com>
2021-11-24 00:48:46 +0800

    final push to finish the day

--- clock.js
+++ clock.js
@@ -1,15 +1,68 @@
-var Clock = function Clock(props) {
-  return React.createElement(
-    'div',
-    null,
-    React.createElement(
-      'h2',
-      null,
-      'The time now is ',
-      props.date.toLocaleTimeString('en-US', { hour12: false }),
-      '.'
-    )
-  );
-};
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-ReactDOM.render(React.createElement(Clock, { date: new Date() }), document.getElementById('root'));
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Clock = function (_React$Component) {
+  _inherits(Clock, _React$Component);
+
+  function Clock(props) {
+    _classCallCheck(this, Clock);
+
+    var _this = _possibleConstructorReturn(this, (Clock.__proto__ || Object.getPrototypeOf(Clock)).call(this, props));
+
+    _this.state = {
+      date: new Date(),
+      location: 'New York'
+    };
+    // this is the component state property object
+    return _this;
+  }
+
+  _createClass(Clock, [{
+    key: 'componentDidMount',
+    value: function componentDidMount() {
+      var _this2 = this;
+
+      this.timer = setInterval(function () {
+        return _this2.updateTime();
+      }, 1000);
+    }
+  }, {
+    key: 'componentWillUnmount',
+    value: function componentWillUnmount() {
+      clearInterval(this.timer);
+    }
+  }, {
+    key: 'updateTime',
+    value: function updateTime() {
+      this.setState({
+        date: new Date()
+      });
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      return React.createElement(
+        'div',
+        null,
+        React.createElement(
+          'h2',
+          null,
+          'The time in ',
+          this.state.location,
+          ' now is ',
+          this.state.date.toLocaleTimeString('en-US', { hour12: false }),
+          '.'
+        )
+      );
+    }
+  }]);
+
+  return Clock;
+}(React.Component); //here is the end of the class
+
+ReactDOM.render(React.createElement(Clock, null), document.getElementById('root'));
--- src/clock.js
+++ src/clock.js
@@ -1,12 +1,41 @@
-const Clock = (props) => {
-    return (
-      <div>
-        <h2>The time now is {props.date.toLocaleTimeString('en-US', {hour12:false})}.</h2>
-      </div>
+class Clock extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = { 
+      date: new Date(),
+      location: 'New York',
+    }; 
+      // this is the component state property object
+  }
+
+  componentDidMount(){
+    this.timer = setInterval(
+      () => this.updateTime(),
+      1000
     );
   }
+
+  componentWillUnmount () {
+    clearInterval(this.timer);
+  }
+
+  updateTime () {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render(){
+    return (
+      <div>
+        <h2>The time in {this.state.location} now is {this.state.date.toLocaleTimeString('en-US', { hour12: false })}.</h2>        
+      </div>
+      )
+    }
+  }//here is the end of the class
   
   ReactDOM.render(
-    <Clock date={new Date()} />,
+    <Clock />,
     document.getElementById('root')
   );
